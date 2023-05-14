a7698b1502605749faa37e48b708976bbeb6e3bb
unknown <thaiduyen0902@gmail.com>
2023-05-15 00:35:50 +0700

    update

--- app/src/main/java/com/example/myblogapp/Activities/Home.java
+++ app/src/main/java/com/example/myblogapp/Activities/Home.java
@@ -1,19 +1,30 @@
 package com.example.myblogapp.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.ImageView;
 import android.widget.TextView;
+//import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 
 import com.bumptech.glide.Glide;
+import com.example.myblogapp.Fragment.HomeFragment;
+import com.example.myblogapp.Fragment.ProfileFragment;
+import com.example.myblogapp.Fragment.SettingsFragment;
 import com.example.myblogapp.R;
 import com.example.myblogapp.databinding.ActivityHome2Binding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -23,10 +34,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener{
 
-    private AppBarConfiguration mAppBarConfiguration;
-    private ActivityHome2Binding binding;
+    //private AppBarConfiguration mAppBarConfiguration;
+    //private ActivityHome2Binding binding;
 
     FirebaseAuth mAuth;
     FirebaseUser currentUser;
@@ -35,7 +47,31 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityHome2Binding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_home2);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //ini
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        });
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        updateNavHeader();
+
+        /*binding = ActivityHome2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         //ini
@@ -55,11 +91,10 @@ public class Home extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        NavigationUI.setupWithNavController(navigationView, navController);*/
 
-
-        updateNavHeader();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -68,15 +103,15 @@ public class Home extends AppCompatActivity {
         return true;
     }
 
-    @Override
+   /* @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
+    }*/
 
     public void updateNavHeader(){
-        NavigationView navigationView = binding.navView;
+        NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
         View headerView = navigationView.getHeaderView(0);
         TextView navUsername = headerView.findViewById(R.id.nav_username);
         TextView navUserMail = headerView.findViewById(R.id.nav_user_mail);
@@ -90,4 +125,67 @@ public class Home extends AppCompatActivity {
         // we need to import the library
         //Glide.with(this).load(currentUser.getPhotoUrl()).into(navUserPhoto);
     }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Handle action bar item clicks here. The action bar will
+        //automatically handle clicks on the Home/Up button, so long
+        //as you spectify a parent activity in AndroidMainifest.xml
+        int id = item.getItemId();
+
+        //noinspection simplifiableIfStatement
+        if (id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        //Handle navigation view item click here
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home){
+
+            getSupportActionBar().setTitle("Home");
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+        } else if (id == R.id.nav_profile) {
+
+            getSupportActionBar().setTitle("Profile");
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, new ProfileFragment()).commit();
+
+        }else if (id == R.id.nav_settings) {
+
+            getSupportActionBar().setTitle("Settings");
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, new SettingsFragment()).commit();
+
+        }else if (id == R.id.nav_signout) {
+
+            FirebaseAuth.getInstance().signOut();
+            Intent loginActivity = new Intent(getApplicationContext(),LoginActivity.class);
+            startActivity(loginActivity);
+            finish();
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    /*@Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }*/
 }
